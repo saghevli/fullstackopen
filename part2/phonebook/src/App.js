6131f090fa5e2b5fc17050fea3e473a1f4ebe5b6
salaamander <saghevli@gmail.com>
2020-09-08 00:14:41 -0400

    2.7 solution

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    if (persons.findIndex(person => person.name === newName) === -1) {
+      setPersons(persons.concat({name: newName}))
+      setNewName('')
+      return
+    }
+    window.alert(`${newName} is already added to phonebook`)
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  } 
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: 
+          <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map((person, index) => <p key={index}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
