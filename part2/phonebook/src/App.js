9832ccd8e09fe3bdb7d5388f35dc369ec13f4139
salaamander <saghevli@gmail.com>
2020-09-08 00:38:38 -0400

    2.9 solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'}
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -29,9 +32,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setNewFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with<input
+        value={newFilter}
+        onChange={handleFilterChange} 
+      />
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: 
@@ -51,7 +64,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map((person, index) => <p key={index}>{person.name} {person.number}</p>)}
+        {persons
+          .filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+          .map((person, index) => <p key={index}>{person.name} {person.number}</p>)}
     </div>
   )
 }
